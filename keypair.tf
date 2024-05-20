d6be15ce4dee72fea3cce62b60e912091bece41f
An Phan <phanan1309@gmail.com>
2024-05-20 10:20:26 +0700

    create keypair with tf

--- keypair.tf
+++ keypair.tf
@@ -1,5 +1,18 @@
-resource "aws_key_pair" "keypair" {
-  key_name = "kp"
-
-  public_key = file("ec2_kp.pub")
+resource "tls_private_key" "keypair" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+  
 }
+
+
+resource "aws_key_pair" "keypair" {
+  key_name = "ec2_kp"
+
+  public_key = tls_private_key.keypair.public_key_openssh
+}
+
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.keypair.key_name}.pem"
+  content = tls_private_key.keypair.private_key_pem
+}
+
